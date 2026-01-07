1cbd920842825e322af3f97a64abeece90be037e
mayank saharan <mayanksaharan@mayanks-MacBook-Air.local>
2026-01-07 10:54:41 +0530

    fix: login API URL interpolation

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -23,7 +23,7 @@ const LoginPage = () => {
 
     try {
       const res = await axios.post(
-        '${process.env.REACT_APP_API_URL}/api/auth/login',
+        `${process.env.REACT_APP_API_URL}/api/auth/login`,
         formData,
         { withCredentials: true }
       );
