7e806a58b77f0d53f6a3d2ef9bcde5488087de60
mayank saharan <mayanksaharan@mayanks-MacBook-Air.local>
2026-01-09 09:50:37 +0530

    fix(admin): correct api import path

--- src/admin/AdminProductsPage.js
+++ src/admin/AdminProductsPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useMemo } from 'react';
-import api from "../utils/api";
+import api from "../services/api";
 import toast, { Toaster } from 'react-hot-toast';
 
 const initialProductForm = {
